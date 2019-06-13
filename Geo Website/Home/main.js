5342d758cfc3b1ee593f1244d76de9880513beb4
Robert MacWha <trebor.ahwcam@gmail.com>
2019-06-13 16:10:35 -0400

    Adding Nova Studios

---
+++ Geo Website/Home/main.js
@@ -0,0 +1,4 @@
+var w = window.innerWidth;
+if (w < 1000) {
+    alert("The window is too small, please increase the width or change to a different computer");
+}
--- Home/main.js
+++
@@ -1,4 +0,0 @@
-var w = window.innerWidth;
-if (w < 1000) {
-    alert("The window is too small, please increase the width or change to a different computer");
-}
