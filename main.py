7b2dfd0e970e54e3719864237520ec1cf2ca08f4
Robert MacWha <trebor.ahwcam@gmail.com>
2020-07-18 22:04:37 -0400

    Adding in more back-front functionality

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from flask import Flask, redirect, url_for, render_template, jsonify, request
 
+template_graphSet = [
+    (0, 1),
+    (10, 20),
+    (2, 30),
+    (30, 40),
+    (4, 50),
+    (50, 60),
+    (6, 70)
+]
+
+template_dates = [
+    "100",
+    "222",
+    "345",
+    "432"
+]
+
+
 app = Flask(__name__,
             static_url_path='', 
             static_folder='web/static',
@@ -8,6 +26,40 @@ app = Flask(__name__,
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/graph<svg>")
+def graph(svg):
+    # Find the max x/y values
+    max_x = 0
+    min_x = 2147483647
+    max_y = 0
+    min_y = 2147483647
+    for j in range(len(template_graphSet)):
+        #Max/min-x
+        if (template_graphSet[j][1] > max_x):
+            max_x=template_graphSet[j][1]
+        if (template_graphSet[j][1] < min_x):
+            min_x = template_graphSet[j][1]
+
+        # Max/min-y
+        if (template_graphSet[j][0] > max_y):
+            max_y=template_graphSet[j][0]
+        if (template_graphSet[j][0] < min_y):
+            min_y = template_graphSet[j][0]
+
+    # Generate the content for an svg element from the given data
+    content = f"<svg class='graph'> <polyline fill='none' stroke='#5cace2' stroke-width='3' points='"
+
+    for i in range(len(template_graphSet)):
+        content += f"{(template_graphSet[i][1])}, {(template_graphSet[i][0] * -1) + max_y} "
+
+    content += "'/>"
+
+    return render_template("graph.html", svgInfo=content)
+
+@app.route("/dates/<date1>/<date2>/<date3>/<date4>")
+def dates(date1, date2, date3, date4):
+    return render_template("dates.html", d1=date1, d2=date2, d3=date3, d4=date4)
 
 @app.route("/environment", methods=["POST", "GET"])
 def environment():
@@ -21,7 +73,8 @@ def environment():
 
             # User inputed year
             year = request.form["y-i1"]
-            return (f"{year}")
+
+            return redirect(url_for("graph", svg="This will be an svg element"))
 
         # User inputs ground settings
         if (selectionMode == "1, 0"):
@@ -31,7 +84,10 @@ def environment():
             aG = request.form["l-i2"]
             fR = request.form["l-i3"]
             uP = request.form["l-i4"]
-            return (f"{pD} + {aG} + {fR} + {uP}")
+
+            dates = [100, 200, 300, 400]; # Set this varible to contain any 4 dates ([x, y, z, w])
+
+            return redirect(url_for("dates", date1=dates[0], date2=dates[1], date3=dates[2], date4=dates[3]))
 
         # User inputs ocean settings
         if (selectionMode == "1, 1"):
@@ -41,7 +97,10 @@ def environment():
             tMP = request.form["o-i2"]
             pH = request.form["o-i3"]
             CO2 = request.form["o-i4"]
-            return (f"{pD} + {tMP} + {pH} + {CO2}")
+
+            dates = template_dates; # Set this varible to contain any 4 dates ([x, y, z, w])
+
+            return redirect(url_for("dates", date1=dates[0], date2=dates[1], date3=dates[2], date4=dates[3]))
 
         # User inputs air settings
         if (selectionMode == "1, 2"):
@@ -51,7 +110,10 @@ def environment():
             nE = request.form["s-i2"]
             cE = request.form["s-i3"]
             mE = request.form["s-i4"]
-            return (f"{pD} + {nE} + {cE} + {mE}")
+
+            dates = template_dates; # Set this varible to contain any 4 dates ([x, y, z, w])
+
+            return redirect(url_for("dates", date1=dates[0], date2=dates[1], date3=dates[2], date4=dates[3]))
 
 
         return (f"{selectionMode}")
